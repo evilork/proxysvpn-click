d9856709dcc1d9a0efac247c1861a4875156283c
Piska-Kiska <alexej.muravyov@gmail.com>
2026-05-09 23:03:49 +0300

    feat: bilingual OG images (EN default + RU for ?lang=ru)

--- app.js
+++ app.js
@@ -31,7 +31,19 @@
              : (SUPPORTED.indexOf(storedLang) >= 0 ? storedLang
              : (SUPPORTED.indexOf(browserLang) >= 0 ? browserLang : 'en'));
 
-    function setLang(l) {
+    
+    // Update OG image based on lang (for SSR-less GitHub Pages, browsers see this on JS-rendered share)
+    function updateOgImage(lang) {
+        const ogImg = document.querySelector('meta[property="og:image"]');
+        const twImg = document.querySelector('meta[name="twitter:image"]');
+        const url = lang === 'ru'
+            ? 'https://proxysvpn.click/og-image-ru.png'
+            : 'https://proxysvpn.click/og-image-en.png';
+        if (ogImg) ogImg.setAttribute('content', url);
+        if (twImg) twImg.setAttribute('content', url);
+    }
+
+function setLang(l) {
         if (SUPPORTED.indexOf(l) < 0) return;
         lang = l;
         try { localStorage.setItem(LANG_KEY, l); } catch (e) {}
